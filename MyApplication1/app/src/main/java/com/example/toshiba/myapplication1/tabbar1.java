755601b56752f93b1fe7f1c49d7f88d53cc0a0db
Nancy Goel <nancygoel62gmail.com>
2016-05-03 00:01:05 +0530

    ListView separate Activity and in tabbars is added

--- MyApplication1/app/src/main/java/com/example/toshiba/myapplication1/tabbar1.java
+++ MyApplication1/app/src/main/java/com/example/toshiba/myapplication1/tabbar1.java
@@ -1,16 +1,28 @@
 package com.example.toshiba.myapplication1;
 
+import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by toshiba on 1/5/16.
  */
+
+
 public class tabbar1 extends Fragment {
 
+    String[] name;
+    String[] hobby;
+    TypedArray profile_pics;
+    List<rowItem> rowItems;
+    ListView mylistview;
 
     public void onCreate(Bundle bundle) {
         super.onCreate(bundle);
@@ -18,6 +30,22 @@ public class tabbar1 extends Fragment {
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle bundle) {
         View v = inflater.inflate(R.layout.tabbar1, container, false);
+        rowItems = new ArrayList<rowItem>();
+
+        name=getResources().getStringArray(R.array.Names);
+        hobby=getResources().getStringArray(R.array.Hobby);
+        profile_pics=getResources().obtainTypedArray(R.array.Profile_Pics);
+
+        for (int i=0;i<name.length;i++)
+        {
+            rowItem item=new rowItem(name[i],profile_pics.getResourceId(i,-1),hobby[i]);
+            rowItems.add(item);
+        }
+
+        mylistview=(ListView)v.findViewById(R.id.listview);
+        CustomAdapter adapter= new CustomAdapter(v.getContext(), rowItems);
+        mylistview.setAdapter(adapter);
+
         return v;
     }
 
